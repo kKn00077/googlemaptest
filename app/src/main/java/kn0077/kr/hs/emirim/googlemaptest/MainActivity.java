0d8379a4f9aed4f346b3c68a6446b73369ffc55e
Kim kyuna <kkn00077@gmail.com>
2017-06-26 20:52:47 +0900

    map type and zoom button and camelalatlngzoom

--- app/src/main/java/kn0077/kr/hs/emirim/googlemaptest/MainActivity.java
+++ app/src/main/java/kn0077/kr/hs/emirim/googlemaptest/MainActivity.java
@@ -3,11 +3,30 @@ package kn0077.kr.hs.emirim.googlemaptest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
+
+    GoogleMap googleMap;
+    SupportMapFragment mapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mapFragment=(SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) { // 구글 맵 서비스가 제공되었을 때
+        this.googleMap=googleMap;
+        googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(35.869130,128.579860),17));
+        googleMap.getUiSettings().setZoomControlsEnabled(true);
     }
 }
